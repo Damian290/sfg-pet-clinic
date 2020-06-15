3618319c8b38d0bc3b00e0302ba8aa45d92235d2
Damian290 <d.klapacz@gmail.com>
2020-06-15 13:38:29 +0200

    Implemented repo

---
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/repositories/SpecialityRepository.java
@@ -0,0 +1,6 @@
+package com.springframework.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialityRepository extends CrudRepository<SpecialityRepository,Long> {
+}
