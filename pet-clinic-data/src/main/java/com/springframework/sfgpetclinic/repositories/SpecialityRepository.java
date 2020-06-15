7092585c82804b6dcbb2794fe6e9e4aa6c6ea960
Damian290 <d.klapacz@gmail.com>
2020-06-15 14:22:18 +0200

    Adding OwnerService (JPA implementation)

--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/repositories/SpecialityRepository.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/repositories/SpecialityRepository.java
@@ -1,6 +1,7 @@
 package com.springframework.sfgpetclinic.repositories;
 
+import com.springframework.sfgpetclinic.model.Speciality;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SpecialityRepository extends CrudRepository<SpecialityRepository,Long> {
+public interface SpecialityRepository extends CrudRepository<Speciality,Long> {
 }
--- pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/springframework/sfgpetclinic/services/OwnerService.java
@@ -5,4 +5,5 @@ import com.springframework.sfgpetclinic.model.Owner;
 public interface OwnerService extends CrudService<Owner,Long> {
 
     Owner findByLastName(String lastName);
+
 }
