f444130eada2e820b5df7d3c357a0c38e301b9e8
Damian290 <d.klapacz@gmail.com>
2020-08-05 11:55:36 +0200

    Display Owner Issue (forgot to commit )

--- pet-clinic-web/src/test/java/com/springframework/sfgpetclinic/Controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/springframework/sfgpetclinic/Controllers/OwnerControllerTest.java
@@ -15,6 +15,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -58,5 +59,14 @@ class OwnerControllerTest {
                 .andExpect(view().name("notimplemented"));
 
         Mockito.verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"));
     }
 }
